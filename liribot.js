7c8dfcd11a6205444bc9c1931c67607578aebc52
kat zhang <kzhang193@gmail.com>
2018-12-09 02:22:12 -0600

    omdb

--- liribot.js
+++ liribot.js
@@ -42,14 +42,16 @@ switch (userChoice) {
         console.log('spotify');
         break;
 
-    // case 'movie-this': 
-    //     movie();
-    //     console.log('movie');
-    //     break;
+    case 'movie-this': 
+        movieInfo();
+        console.log('movie');
+        break;
+
+    /// Switch-case for readFile fx
     case 'do-what-it-says':
     whatever();
     break;
-
+    /// If user does not type anything in command line after "node liribot"
     default:
         console.log('please enter something!');
         break;
@@ -66,9 +68,9 @@ function concertInfo() {
     axios.get(queryURL).then(
         function(response) {
          for (i = 0 ; i < response.data.length; i++) {
-           console.log("Venue Name: ", response.data[i].venue.name);
-           console.log("Date & Time: ", moment(response.data[i].datetime).format('LLL'));
-           console.log("Location: ", response.data[i].venue.city + ", " + response.data[i].venue.country)
+           console.log("- Venue Name: ", response.data[i].venue.name);
+           console.log("- Date & Time: ", moment(response.data[i].datetime).format('LLL'));
+           console.log("- Location: ", response.data[i].venue.city + ", " + response.data[i].venue.country)
            console.log("---------------------------------------");
         }
         }
@@ -76,27 +78,17 @@ function concertInfo() {
 }
 
 function songInfo() {
-//     var songName = process.argv[3];
-//     if (process.argv[3]=== undefined) {
-//         songName = "Ace of Base"
 
-//     }
-//     else {
-//         songName === process.argv[3]
-    
-//     for (let i = 4; i < process.argv.length; i++) {
-//         songName += " " + process.argv[i];
-
-//     }
-// }
     spotify
         .search({ type: 'track', query: songName, limit: 1 })
         .then(function (response) {
         
-            console.log("Artist Name: ", response.tracks.items[0].album.artists[0].name);
-            console.log("PreviewLink: ", response.tracks.items[0].album.external_urls.spotify);
-            console.log("Featured Album: ", response.tracks.items[0].album.name);
-            console.log("Song Title: ", response.tracks.items[0].name);
+            console.log("- Song Title: ", response.tracks.items[0].name);
+            console.log("- Artist Name: ", response.tracks.items[0].album.artists[0].name);
+            console.log("- Featured Album: ", response.tracks.items[0].album.name);
+            console.log("- Preview Link: ", response.tracks.items[0].album.external_urls.spotify);
+            
+            
 
 
         })
@@ -104,6 +96,28 @@ function songInfo() {
             console.log(err);
         });
 }
+
+
+function movieInfo() {
+    var movieTitle = process.argv[3];
+    for (let i = 4; i < process.argv.length; i++) {
+        movieTitle += " " + process.argv[i];
+    }
+    var queryURL = `http://www.omdbapi.com/?t=${movieTitle}&y=&plot=short&apikey=${keys.movies.id}`;
+    axios.get(queryURL).then(
+        function(response) {
+            console.log("- Movie Title: " + response.data.Title);
+        console.log("- Release Year: " + response.data.Year);
+        console.log("- IMDB Rating: " + response.data.Ratings[0].Value);
+        console.log("- Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("- Produced in: " + response.data.Country);
+        console.log("- Language: " + response.data.Language);
+        console.log("- Plot Summary: " + response.data.Plot);
+        console.log("- Lead Actors: " + response.data.Actors);
+        }
+    )
+}
+
 
 function whatever() {
     fs.readFile("random.txt", "utf8", function (error, data) {
